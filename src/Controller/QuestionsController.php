62455a4d0e06bbbb65834618e59c157bfb1b99af
aliii390 <alli.said.errahmani@gmail.com>
2025-03-06 15:47:36 +0100

    add: fin des category

--- src/Controller/QuestionsController.php
+++ src/Controller/QuestionsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +11,11 @@ use Symfony\Component\Routing\Attribute\Route;
 final class QuestionsController extends AbstractController
 {
     #[Route('/questions', name: 'app_questions')]
-    public function index(): Response
+    public function index(CategoryRepository $categoryRepo): Response
     {
+        $category =  $categoryRepo->findAll();
         return $this->render('questions/index.html.twig', [
-            'controller_name' => 'QuestionsController',
+            'category' => $category,
         ]);
     }
 }
