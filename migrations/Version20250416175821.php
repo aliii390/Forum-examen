8e6eae238337b6f4633577973e50217a2af69962
aliii390 <allli.said.errahmani@gmail.com>
2025-04-16 20:38:56 +0200

    add :fin des modif

--- migrations/Version20250416175821.php
+++ migrations/Version20250416175821.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250416090653 extends AbstractMigration
+final class Version20250416175821 extends AbstractMigration
 {
     public function getDescription(): string
     {
